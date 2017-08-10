2199f0ec4af13bb5939de81a1c4973a95e5c7bb0
github username <github email address>
2017-08-10 09:59:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// code solution here
+// // code solution here
+// class User {
+//   constructor(name, email) {
+//     this.name = name;
+//     this.email = email;
+//   }
+//
+//   sayHello() {
+//     console.log("Hello, my name is "+ this.name);
+//   }
+// }
+
+class President {
+  constructor(name, politicalParty, yearsInOffice, homeState) {
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState;
+  }
+
+  veto() {
+    return "NO!";
+  }
+
+  passBill() {
+    return "You can do that!";
+  }
+
+  doCharity(){
+    return "I like to help people.";
+  }
+
+  conductPressInterview() {
+    return "I am proud to be an American.";
+  }
+
+  sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}. I represent the ${this.politicalParty}s, and was in office ${this.yearsInOffice}.`
+  }
+}
+
+var george = new President("George Washington");
+var abraham = new President("Abraham Lincoln");
+var richard = new President("Richard Nixon");
+var jimmy = new President("Jimmy Carter", "Democrat", 4, "Georgia");
